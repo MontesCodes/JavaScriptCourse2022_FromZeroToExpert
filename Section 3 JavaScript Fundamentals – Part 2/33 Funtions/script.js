3912d4aa0a117962b3f6153eac1149d8241500b9
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-05-14 21:30:01 -0400

    updating JS files #1

--- Section 3 JavaScript Fundamentals – Part 2/33 Funtions/script.js
+++ Section 3 JavaScript Fundamentals – Part 2/33 Funtions/script.js
@@ -10,7 +10,7 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
+  //console.log(apples, oranges);
   const juice = 'Juice with ${apples} apples and ${oranges} oranges.';
   return juice;
 }
@@ -20,3 +20,6 @@ console.log(appleJuice);
 console.log(fruitProcessor(5, 1));
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+const num = number('23');
+console.log(num);
