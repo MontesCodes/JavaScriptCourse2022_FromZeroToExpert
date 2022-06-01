c36915d612c121f217afcf38a4a8ee0d480dfbb7
MontesCodes-CodingTheFrontEnd <montiesWebDev@gmail.com>
2022-05-31 21:52:57 -0400

    JS update Functions Calling Other Functions 5/31/22 #3

--- Section 3 JavaScript Fundamentals – Part 2/36FunctionsCallingOtherFunctions/FunctionsCallingOtherFun.js
+++ Section 3 JavaScript Fundamentals – Part 2/36FunctionsCallingOtherFunctions/FunctionsCallingOtherFun.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function cutFruitPieces(fruit)
+
 function fruitProcessor(apples, oranges) {
   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
