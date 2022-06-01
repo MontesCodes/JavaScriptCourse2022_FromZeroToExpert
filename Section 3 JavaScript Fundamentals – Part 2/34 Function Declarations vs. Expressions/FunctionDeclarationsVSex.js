45a68748813374fae05706912d4c215525234634
MontesCodes-CodingTheFrontEnd <montiesWebDev@gmail.com>
2022-05-31 20:56:25 -0400

    JS update Function Expresion 5/31/22 #2

--- Section 3 JavaScript Fundamentals – Part 2/34 Function Declarations vs. Expressions/FunctionDeclarationsVSex.js
+++ Section 3 JavaScript Fundamentals – Part 2/34 Function Declarations vs. Expressions/FunctionDeclarationsVSex.js
@@ -1,11 +1,13 @@
 'use strict';
-/// Function declaration
+/// Function declaration - before or after function
+const age3 = calcAge1(1984);
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 }
 
-const age1 = calcAge1(1984);
+const age1 = calcAge1(1983);
 console.log(age1);
+console.log(age3);
 
 /// Function exprssion
 const calcAge2 = function (birthYear) {
