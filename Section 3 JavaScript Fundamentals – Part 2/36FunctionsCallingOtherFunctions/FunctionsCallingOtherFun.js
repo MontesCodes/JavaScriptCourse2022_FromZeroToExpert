d5eabf39d9f83bbd9642f9f8795a2f2244b0262a
MontesCodes-CodingTheFrontEnd <montiesWebDev@gmail.com>
2022-05-31 21:22:16 -0400

    JS update Functions Calling Other Functions 5/31/22 #2

--- Section 3 JavaScript Fundamentals – Part 2/36FunctionsCallingOtherFunctions/FunctionsCallingOtherFun.js
+++ Section 3 JavaScript Fundamentals – Part 2/36FunctionsCallingOtherFunctions/FunctionsCallingOtherFun.js
@@ -1,19 +1,6 @@
-"use strict";
+'use strict';
 
-// const calcAge = function (birthYear) {
-//   return 2037 - birthYear;
-// };
-
-// const yearsUntilRetirement = function (birthYear, firstName) {
-//   const age = calcAge(birthYear);
-//   const retirement = 65 - age;
-
-//   if (retirement > 0) {
-//     return retirement;
-//   } else {
-//     return -1;
-//   }
-// };
-
-// console.log(yearsUntilRetirement(1991, "Jonas"));
-// console.log(yearsUntilRetirement(1950, "Mike"));
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
