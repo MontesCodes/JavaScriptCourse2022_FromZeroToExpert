736c4fd5b437863c879b29b6b26ed7dbb7cb3893
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-06-01 20:52:15 -0400

    JS Functions Calling other Functions, updating from Mac desktop 6/1/22 #1

--- Section 3 JavaScript Fundamentals – Part 2/36FunctionsCallingOtherFunctions/FunctionsCallingOtherFun.js
+++ Section 3 JavaScript Fundamentals – Part 2/36FunctionsCallingOtherFunctions/FunctionsCallingOtherFun.js
@@ -1,8 +1,13 @@
 'use strict';
 
-function cutFruitPieces(fruit)
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
 
 function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} oranges.`;
   return juice;
 }
+console.log(fruitProcessor(10, 5));
