70a5f8fee05e64dd0d9a044f44d794a9ae75e082
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-06-01 21:13:08 -0400

    JS - Reviewing Functions, MacBookPro desktop 6/1/22 #1

---
+++ Section 3 JavaScript Fundamentals – Part 2/37. Reviewing Functions/reviewingFunctions.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+
+  return `${firstName} retires in ${retirement} years.`;
+};
