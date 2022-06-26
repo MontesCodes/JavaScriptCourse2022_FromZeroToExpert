c9e7cab61f500b8878683fc82017bd902f2a377f
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-06-25 21:45:03 -0400

    updating JS, MacBookPro desktop 6/25/22 #2

--- Section 3 JavaScript Fundamentals – Part 2/37. Reviewing Functions/reviewingFunctions.js
+++ Section 3 JavaScript Fundamentals – Part 2/37. Reviewing Functions/reviewingFunctions.js
@@ -8,7 +8,14 @@ const yearsUntilRetirement = function (birthYear, firstName) {
   const age = calcAge(birthYear);
   const retirement = 65 - age;
 
-  return retirement;
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired.`);
+    return -1;
+  }
   // return `${firstName} retires in ${retirement} years.`;
 };
 console.log(yearsUntilRetirement(1991, 'Monties'));
+console.log(yearsUntilRetirement(1970, 'Joe'));
