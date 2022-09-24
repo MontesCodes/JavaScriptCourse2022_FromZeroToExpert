919dbb83d96772aa6bc8b93634fb844873b541f6
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-09-24 18:50:57 -0400

    finished 10 Values and Variables, MacBookPro 9/24/2022

---
+++ Section 2 JavaScript Fundamentals – Part 1/10 Values and Variables/script.js
@@ -0,0 +1,22 @@
+'use strict';
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log(23);
+
+let firstName = 'MontesCodes';
+
+console.log(firstName);
+
+let montes_matilda = 'MM';
+let $function = 27;
+
+let person = 'montes';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
