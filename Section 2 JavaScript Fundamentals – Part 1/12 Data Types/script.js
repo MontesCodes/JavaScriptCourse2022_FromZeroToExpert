2d756908ffe2199687c599399e0cbdb32ede6796
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-09-24 19:56:36 -0400

    finished 12 Data Types, MacBookPro 9/24/2022

---
+++ Section 2 JavaScript Fundamentals – Part 1/12 Data Types/script.js
@@ -0,0 +1,43 @@
+'use strict';
+// let js = 'amazing';
+/* console.log(40 + 8 + 23 - 10);
+console.log(23);
+
+let firstName = 'MontesCodes';
+
+console.log(firstName);
+
+let montes_matilda = 'MM';
+let $function = 27;
+
+let person = 'montes';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob); */
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'MontesCodes');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
