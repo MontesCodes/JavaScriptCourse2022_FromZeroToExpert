e2d75c3695248325684442bcf4e4e48bc974802d
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-09-24 20:14:08 -0400

    finished 13 let const and var, MacBookPro 9/24/2022

---
+++ Section 2 JavaScript Fundamentals – Part 1/13 let const and var/script.js
@@ -0,0 +1,58 @@
+'use strict';
+// let js = 'amazing';
+/* console.log(40 + 8 + 23 - 10);
+console.log(23);
+
+let firstName = 'MontesCodes';
+
+console.log(firstName);
+
+let montes_matilda = 'MM';
+let $function = 27;
+
+let person = 'montes';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob); */
+
+/* let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'MontesCodes');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); */
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+console.log(job);
+
+lastName = 'Kelly';
+console.log(lastName);
