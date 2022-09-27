8308a3d98bcdfec92662e7b40b96f58a5953c1a6
MontiesCodes_1557 <MontiesWebDev@Gmail.com>
2022-09-27 08:11:37 -0400

    finished 16 Coding Challenge

--- Section 2 JavaScript Fundamentals – Part 1/16 Coding Challenge/script.js
+++ Section 2 JavaScript Fundamentals – Part 1/16 Coding Challenge/script.js
@@ -104,3 +104,6 @@ const heightJohn = 1.95;
 
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
