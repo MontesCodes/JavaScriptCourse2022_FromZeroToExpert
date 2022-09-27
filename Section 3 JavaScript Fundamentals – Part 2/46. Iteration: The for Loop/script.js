d163d2cb23901fad227e7aaaec23358ac3220e44
MontiesCodes_1557 <MontiesWebDev@Gmail.com>
2022-09-27 08:05:19 -0400

    updating 16 Coding Challenge

--- Section 3 JavaScript Fundamentals – Part 2/46. Iteration: The for Loop/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-for (let rep = 1; rep <= 10; rep++) {
-  console.log('Lifting weights repetition 1 ');
-}
-
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights repetition ${rep} `);
-}
