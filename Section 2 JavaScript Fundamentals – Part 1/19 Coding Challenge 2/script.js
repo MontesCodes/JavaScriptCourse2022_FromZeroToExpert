3d34cb7de67279c27914fe374854601108f85d3c
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2022-10-04 08:00:42 -0400

    updating 19 Coding Chalenge 2, EngineeringMe, 10-04-2022 #1

--- Section 2 JavaScript Fundamentals – Part 1/19 Coding Challenge 2/script.js
+++ Section 2 JavaScript Fundamentals – Part 1/19 Coding Challenge 2/script.js
@@ -1,1 +1,21 @@
 'use strict';
+// Coding Challenge #2
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log("Mark's BMI is higher than John's!");
+} else {
+  console.log("john's BMI is higher than Marks's!");
+}
