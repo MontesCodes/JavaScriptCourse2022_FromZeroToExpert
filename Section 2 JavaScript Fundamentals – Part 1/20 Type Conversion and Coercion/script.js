0f69a07ac4a38d59045b5d1149e3a6d89175b096
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2022-10-15 16:45:13 -0400

    updating 20 Type Conversion and Coercion, EngineeringMe, 10-15-2022

--- Section 2 JavaScript Fundamentals – Part 1/20 Type Conversion and Coercion/script.js
+++ Section 2 JavaScript Fundamentals – Part 1/20 Type Conversion and Coercion/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// type Conversion
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(inputYear + 18);
@@ -7,3 +8,6 @@ console.log(Number(inputYear) + 18);
 console.log(Number('Montes'));
 console.log(typeof NaN);
 console.log(String(23), 23);
+
+// Type Coercion
+console.log('I am' + 23 +);
