3cd670fcd16d0dac5cb8610c163156b5a5cf42f0
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-26 19:31:14 -0400

    updating 22 Equality Operators, MacBookPro 10/26/2022 #2

--- Section 2 JavaScript Fundamentals – Part 1/22 Equality Operators/script.js
+++ Section 2 JavaScript Fundamentals – Part 1/22 Equality Operators/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favorite = prompt("What's your favorite number?");
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite == 23) {
+  console.log('Cool! 23 is an amazing number!');
+}
