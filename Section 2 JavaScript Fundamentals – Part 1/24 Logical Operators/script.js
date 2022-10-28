47dbe67c7d30012b16c5f82777e41d59f0122f82
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-27 20:14:35 -0400

    Finished 24 Logical Operators, MacBookPro 10/27/2022

--- Section 2 JavaScript Fundamentals – Part 1/24 Logical Operators/script.js
+++ Section 2 JavaScript Fundamentals – Part 1/24 Logical Operators/script.js
@@ -5,3 +5,20 @@ const hasGoodVision = true; // B
 console.log(hasDriversLicense && hasGoodVision);
 console.log(hasDriversLicense || hasGoodVision);
 console.log(!hasGoodVision);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (hasDriversLicense && hasGoodVision) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}
