7f3676ec13b8078a1dbe9545b2a14044313aae66
MontesCode_EngineeringMeLap <MontiesWebDev@Gmail.com>
2022-11-01 07:47:54 -0400

    updating 25 Coding Challenge 3, EngineeringMe, 11-01-2022

--- Section 2 JavaScript Fundamentals – Part 1/25 Coding Challenge 3/script.js
+++ Section 2 JavaScript Fundamentals – Part 1/25 Coding Challenge 3/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log('Dolphins win the trophy');
+}
